1ac59f99374c505e515d762c1efd59d40455d7ef
Alyssa Batula <batulaa@drexel.edu>
2013-10-09 19:13:10 -0400

    Switched from sending char to short over TCP/IP. Also fixed connection error

--- RobotTester.py
+++ RobotTester.py
@@ -1,7 +1,8 @@
 import socket
 import struct
 
-#clientAddr = '192..168.123.1' # Address of the machine receiving commands
+#clientAddr = '192.168.123.1' # Address of the machine receiving commands
+clientAddr = ''
 clientPort = 60000
 
 tmp = input('Press enter when ready to connect')
@@ -10,11 +11,14 @@ tmp = input('Press enter when ready to connect')
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 s.bind((clientAddr, clientPort))
-s.listen(10)
+s.listen(1)
 clientSock, addr = s.accept()
 
 sendChar = None
 while sendChar != '0':
     sendChar = input('Enter command [1-4] or 0 to quit: ')
-    msg = struct.pack(sendChar, '<c')
-    clientSock.sendall(msg)
+    if sendChar in '01234':
+        msg = struct.pack('<h', int(sendChar))
+        clientSock.sendall(msg)
+    else:
+        print('Invalid character')
